95551b34f010bf70b0a767eca8f35828add712ff
balumaqsud <ollobergan008@mail.ru>
2025-04-06 16:35:00 +0900

    feat: navbar impemented

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,18 @@
-import React, { ReactNode } from 'react'
+import Link from "next/link";
+import Image from "next/image";
+import React, { ReactNode } from "react";
 
-const RootLayout = ({children}: {children: ReactNode}) => {
+const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div>{children}</div>
-  )
-}
-export default RootLayout
+    <div className="root-layout">
+      <nav>
+        <Link href={"/"} className="flex items-center gap-2">
+          <Image src="/logo.png" alt="logo" height={48} width={36}></Image>
+          <h2 className="text-primary-100">SpeakWise</h2>
+        </Link>
+      </nav>
+      {children}
+    </div>
+  );
+};
+export default RootLayout;
